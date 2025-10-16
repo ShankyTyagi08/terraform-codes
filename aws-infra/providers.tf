58519d4de046b041f6997c0a2297c4a6faa6675c
Shankytyagi <Shankytyagi@gmail.com>
2025-10-16 11:09:22 +0530

    provider update

--- aws-infra/providers.tf
+++ aws-infra/providers.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 
   assume_role {
-    role_arn     = "arn:aws:iam::204917013198:role/TerraformExecutionRole"
+    role_arn     = "arn:aws:iam::204917013198:role/Terraform"
     session_name = "tf-apply"
   }
 }
